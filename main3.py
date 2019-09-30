2a2036a0667a3bdbc3f7a0ef190da20f14ca7bb6
wrednaya33 <novikova@yandexlyceum.ru>
2019-09-30 16:59:05 +0400

    Изменение цвета фона и рисование белой полосы

--- main3.py
+++ main3.py
@@ -3,10 +3,11 @@ import pygame
 pygame.init()
 
 scr = pygame.display.set_mode((300, 300))
-scr.fill((255, 255, 255))
+scr.fill(pygame.Color('green'))
 
 def draw():
     pygame.draw.rect(scr, pygame.Color('black'), [(10, 10), (20, 260)], 0)
+    pygame.draw.rect(scr, pygame.Color('white'), [(30, 10), (170, 60)], 0)
 
 draw()
 
